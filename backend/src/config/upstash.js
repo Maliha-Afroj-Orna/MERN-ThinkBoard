ad0a04fe4366d262f831611814c018f26d775f05
Maliha Afroj Orna <63250354+Maliha-Afroj-Orna@users.noreply.github.com>
2025-10-08 18:54:24 +0600

    initial commit

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,10 @@
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis';
+
+// Create a ratelimiter that allows 100 requests per 60 seconds
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(10, '20 s')
+})
+
+export default ratelimit;
